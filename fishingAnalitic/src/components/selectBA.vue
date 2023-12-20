2980390531c46e77ea121bfde29aaa5c6f9642c3
Alex Mur <lehamuraschov2014@yandex.ru>
2023-12-20 21:00:53 +0200

    feat: delete two inputs and create folder backend

--- fishingAnalitic/src/components/selectBA.vue
+++ fishingAnalitic/src/components/selectBA.vue
@@ -7,47 +7,58 @@
       <q-select v-model="modelFish" :options="optionsFish" label="Вид рыбы" />
     </div>
     <div class="q-gutter-md">
-      <q-select v-model="modelSize" :options="optionsSize" label="Размер рыбы, кг" />
+      <q-select
+        v-model="modelSize"
+        :options="optionsSize"
+        label="Размер рыбы, кг"
+      />
     </div>
     <div class="q-gutter-md">
       <q-select v-model="modelBait" :options="optionsBait" label="Приманка" />
     </div>
     <div class="q-gutter-md">
-      <q-select v-model="modelSizeBait" :options="optionsSizeBait" label="Размер приманки, см" />
+      <q-select
+        v-model="modelSizeBait"
+        :options="optionsSizeBait"
+        label="Размер приманки, см"
+      />
     </div>
     <div class="q-gutter-md">
-      <q-select v-model="modelFish" :options="optionsFish" label="Вид рыбы" />
-    </div>
-    <div class="q-gutter-md">
-      <q-select v-model="modelSize" :options="optionsSize" label="Размер" />
-    </div>
-    <div class="q-gutter-md">
-      <q-select v-model="modelBait" :options="optionsBait" label="Приманка" />
+      <q-select v-model="modelColor" :options="optionsColor" label="Цвет" />
     </div>
   </div>
 </template>
 
 <script>
-import { ref } from 'vue'
+import { ref } from "vue";
 
 export default {
-  setup () {
+  setup() {
     return {
       modelFish: ref(null),
-      modelPlace:  ref(null),
-      modelSize:  ref(null),
+      modelPlace: ref(null),
+      modelSize: ref(null),
       modelBait: ref(null),
       modelSizeBait: ref(null),
-      optionsFish: [
-        'щука', 'судак', 'окунь', 'жерех'
+      optionsFish: ["щука", "судак", "окунь", "жерех"],
+      optionsPlace: ["место"],
+      optionsSize: [
+        "0.1",
+        "0.2",
+        "0.3",
+        "0.4",
+        "0.5",
+        "0,7",
+        "1.0",
+        "1.2",
+        "1.5",
+        "1.8",
+        "2.0",
       ],
-      optionsPlace:['место'],
-      optionsSize: ['0.1', '0.2', '0.3', '0.4', '0.5', '0,7', '1.0', '1.2', '1.5',
-    '1.8', '2.0'],
-    optionsBait: ['тьега', 'гамблер'],
-    optionsSizeBait: ['1', '2', '3', '4', '5', '6', '7', '8', '9', '10'],
-
-    }
-  }
-}
+      optionsBait: ["тьега", "гамблер"],
+      optionsSizeBait: ["1", "2", "3", "4", "5", "6", "7", "8", "9", "10"],
+      optionsColor: ["зеленый", "белый", "красный"],
+    };
+  },
+};
 </script>
