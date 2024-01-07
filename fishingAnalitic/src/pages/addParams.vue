af80e796480d64b0111e67269d2ac9df9aefbc67
Alex Mur <lehamuraschov2014@yandex.ru>
2024-01-07 11:20:08 +0200

    feat: create example cicle for button for method

--- fishingAnalitic/src/pages/addParams.vue
+++ fishingAnalitic/src/pages/addParams.vue
@@ -3,8 +3,13 @@
     <div>
       <h5 class="centered-text">Вы можете добавить свой параметр:</h5>
     </div>
-    <div v-for="i in labelButton" :key="i">
-      <q-btn size="lg" color="primary" :label="i" />
+    <div v-for="i in button" :key="i">
+      <q-btn
+        size="lg"
+        color="primary"
+        :label="i.label"
+        @click="callM(i.method)"
+      />
     </div>
   </q-page>
 </template>
@@ -13,13 +18,32 @@
 import buttonAdd from "../components/buttonAdd.vue";
 import { ref } from "vue";
 
-const labelButton = [
-  "место",
-  "вид рыбы",
-  "приманка",
-  "цвет приманки",
-  "размер приманки",
+const button = [
+  { label: "место", method: "m1" },
+  { label: "вид рыбы", method: "m2" },
+  { label: "приманка", method: "m3" },
+  { label: "цвет приманки", method: "m4" },
+  { label: "размер приманки", method: "m5" },
 ];
+
+function m1() {
+  console.log("m1");
+}
+function m2() {
+  console.log("m2");
+}
+function m3() {
+  console.log("m3");
+}
+function m4() {
+  console.log("m4");
+}
+function m5() {
+  console.log("m5");
+}
+function callM(m) {
+  this[m]();
+}
 </script>
 
 <style scoped>
@@ -39,6 +63,6 @@ button {
   text-justify: inter-word;
   margin: 10px;
   font-weight: bold;
-  color: rgb(3, 3, 59);
+  color: rgb(71, 71, 64);
 }
 </style>
