8daa658a548579b1c5e39e579e3a37f82d79a5e8
Alex Mur <lehamuraschov2014@yandex.ru>
2024-11-08 22:22:07 +0200

    delete button from selectBA.vue

--- fishingAnalitic/src/components/selectBA.vue
+++ fishingAnalitic/src/components/selectBA.vue
@@ -26,26 +26,36 @@
     <div class="q-gutter-md">
       <q-select v-model="modelColor" :options="optionsColor" label="Цвет" />
     </div>
-    <q-btn class="q-mt-md" color="primary" label="Отправить" type="submit"></q-btn>
+    <q-btn
+      class="q-mt-md"
+      color="primary"
+      label="Отправить"
+      type="submit"
+    ></q-btn>
   </div>
-  <q-btn @click = 'console.log(user)' class="q-mt-md" color="primary" label="Отправить" type="submit"></q-btn>
-
 </template>
 
 <script setup>
 import { ref } from "vue";
-import { inputOptions } from '../stores/input-store.js';
+import { inputOptions } from "../stores/input-store.js";
 import { storeToRefs } from "pinia";
 
 const optionStore = inputOptions();
-const {optionsFish, optionsPlace, optionsBait, optionsColor, optionsSize,
-   optionsSizeBait, user} = storeToRefs(optionStore);
-const {getUser} = optionStore;
+const {
+  optionsFish,
+  optionsPlace,
+  optionsBait,
+  optionsColor,
+  optionsSize,
+  optionsSizeBait,
+  user,
+} = storeToRefs(optionStore);
+const { getUser } = optionStore;
 getUser();
-  const modelFish = ref(null);
-  const modelPlace = ref(null);
-  const modelSize = ref(null);
-  const modelBait = ref(null);
-  const modelSizeBait = ref(null);
-  const modelColor = ref(null);
+const modelFish = ref(null);
+const modelPlace = ref(null);
+const modelSize = ref(null);
+const modelBait = ref(null);
+const modelSizeBait = ref(null);
+const modelColor = ref(null);
 </script>
