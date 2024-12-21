1da9ac41e8f3123051833f6a152923a6641aefdf
Alex Mur <lehamuraschov2014@yandex.ru>
2024-12-21 16:03:30 +0200

    feat: install bcrypt and first try

--- backend/server.js
+++ backend/server.js
@@ -1,25 +1,28 @@
-const { json } = require('express');
 const express = require('express');
-const {connectToDb, getDb} = require('./db');
+const mongoose = require('mongoose');
+const bcrypt = require('bcrypt');
+
+const hash = bcrypt.hashSync('df', 10);
+console.log(hash);
 
 const PORT = 3001;
+const URL = "mongodb://localhost:27017/fish";
 
 const app = express();
-app.use(express.json());
+// app.use(express.json());
+
+
+
+mongoose
+  .connect(URL)
+  .then(() => console.log('Connected to MongoDB'))
+  .catch((err) => console.log(`DB connection error ${err}`));
+
+  app.listen(PORT, (err)=>{
+    err ? console.log(err) : console.log(`Listening port ${PORT}`);
+});
 
 let db;
-
-connectToDb((err) =>{
-    if(!err){
-        app.listen(PORT, (err)=>{
-            err ? console.log(err) : console.log(`Listening port ${PORT}`);
-        });
-        db = getDb();
-    }
-    else{
-        console.log(`DB connection error: ${err}`);
-    }
-})
 
 const handelError = (res, error) => {
     res.status(500).json({error});
